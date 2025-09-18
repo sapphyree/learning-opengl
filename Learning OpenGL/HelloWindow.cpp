0213b3ff52ea31079fedc5d8ba23d99b3b0c350f
Sapphyre <75276796+sapphyree@users.noreply.github.com>
2025-09-18 13:11:27 +0100

    Create git repository.

---
+++ Learning OpenGL/HelloWindow.cpp
@@ -0,0 +1,66 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <string>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void error_callback(int error, const char* msg);
+void processInput(GLFWwindow* window);
+
+int main() {
+	glfwInit();
+	glfwSetErrorCallback(error_callback);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialise GLAD" << std::endl;
+		return -1;
+	}
+
+	// rendering loop
+	while (!glfwWindowShouldClose(window)) {
+		// input processing
+		processInput(window);
+
+		// rendering code
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// check & call events & swap buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+void error_callback(int error, const char* msg) {
+	std::string s;
+	s = " [" + std::to_string(error) + "] " + msg + '\n';
+	std::cerr << s << std::endl;
+}
+
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
+}
