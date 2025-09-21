340e30543bdae7c5ac884e5e3c50152bee280e77
Sapphyre <75276796+sapphyree@users.noreply.github.com>
2025-09-21 17:38:10 +0100

    Add namespace for HelloTriangle to allow for next chapter file.

--- Learning OpenGL/HelloTriangle.cpp
+++ Learning OpenGL/HelloTriangle.cpp
@@ -4,233 +4,236 @@
 #include <string>
 #include <iostream>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void error_callback(int error, const char* msg);
-void processInput(GLFWwindow* window);
+namespace  HelloTriangle {
 
-// vertex shader in glsl
-const char* vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"void main()\n"
-"{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"}\0";
-const char* fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-"}\n\0";
-const char* yellowFragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-"   FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n"
-"}\n\0";
+	void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+	void error_callback(int error, const char* msg);
+	void processInput(GLFWwindow* window);
 
-int main() {
+	// vertex shader in glsl
+	const char* vertexShaderSource = "#version 330 core\n"
+		"layout (location = 0) in vec3 aPos;\n"
+		"void main()\n"
+		"{\n"
+		"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+		"}\0";
+	const char* fragmentShaderSource = "#version 330 core\n"
+		"out vec4 FragColor;\n"
+		"void main()\n"
+		"{\n"
+		"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+		"}\n\0";
+	const char* yellowFragmentShaderSource = "#version 330 core\n"
+		"out vec4 FragColor;\n"
+		"void main()\n"
+		"{\n"
+		"   FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n"
+		"}\n\0";
 
-	// glfw initialise + config
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	int main() {
 
-	// glfw window creation
-	GLFWwindow* window = glfwCreateWindow(800, 600, "HelloTriangle", NULL, NULL);
-	if (window == NULL) {
-		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		return -1;
-	}
-	glfwMakeContextCurrent(window);
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+		// glfw initialise + config
+		glfwInit();
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	// glfw load function pointers
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-		std::cout << "Failed to initialise GLAD" << std::endl;
-		return -1;
-	}
+		// glfw window creation
+		GLFWwindow* window = glfwCreateWindow(800, 600, "HelloTriangle", NULL, NULL);
+		if (window == NULL) {
+			std::cout << "Failed to create GLFW window" << std::endl;
+			glfwTerminate();
+			return -1;
+		}
+		glfwMakeContextCurrent(window);
+		glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	// shader creation
-	// create shader GL object, provide the shader source and compile
-	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	glCompileShader(vertexShader);
+		// glfw load function pointers
+		if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+			std::cout << "Failed to initialise GLAD" << std::endl;
+			return -1;
+		}
 
-	// compile time error checking
-	int success;
-	char infoLog[512];
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-	if (!success) {
-		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-	}
+		// shader creation
+		// create shader GL object, provide the shader source and compile
+		unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+		glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+		glCompileShader(vertexShader);
 
-	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
+		// compile time error checking
+		int success;
+		char infoLog[512];
+		glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+		if (!success) {
+			glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+			std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+		}
 
-	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-	if (!success) {
-		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-	}
+		unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+		glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+		glCompileShader(fragmentShader);
 
-	// ex3: yellow fragment shader
-	unsigned int yellowFragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(yellowFragmentShader, 1, &yellowFragmentShaderSource, NULL);
-	glCompileShader(yellowFragmentShader);
+		glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+		if (!success) {
+			glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+			std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+		}
 
-	// shader program and shader linking
-	// links the outputs of each shader to the inputs of the next shader
-	unsigned int shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
+		// ex3: yellow fragment shader
+		unsigned int yellowFragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+		glShaderSource(yellowFragmentShader, 1, &yellowFragmentShaderSource, NULL);
+		glCompileShader(yellowFragmentShader);
 
-	// compile time checking for linking
+		// shader program and shader linking
+		// links the outputs of each shader to the inputs of the next shader
+		unsigned int shaderProgram = glCreateProgram();
+		glAttachShader(shaderProgram, vertexShader);
+		glAttachShader(shaderProgram, fragmentShader);
+		glLinkProgram(shaderProgram);
 
-	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-	if (!success) {
-		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-	}
+		// compile time checking for linking
 
-	// shader program 2
-	unsigned int shaderProgramWithYellow = glCreateProgram();
-	glAttachShader(shaderProgramWithYellow, vertexShader);
-	glAttachShader(shaderProgramWithYellow, yellowFragmentShader);
-	glLinkProgram(shaderProgramWithYellow);
+		glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+		if (!success) {
+			glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+			std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+		}
 
-	// delete shader objects after linking
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
-	glDeleteShader(yellowFragmentShader);
+		// shader program 2
+		unsigned int shaderProgramWithYellow = glCreateProgram();
+		glAttachShader(shaderProgramWithYellow, vertexShader);
+		glAttachShader(shaderProgramWithYellow, yellowFragmentShader);
+		glLinkProgram(shaderProgramWithYellow);
 
-	// vertex data for input to shader pipeline, renders a triangle
-	// data is x,y,z coordinate space, values must be between -1.0 and 1.0
-	// coordinate space is normalised (i.e all values > 1 or < -1 will be outside the screen space) and is converted to screen-space coordinates via the viewport transform function
-	// z coordinate is 0.0f as it wants to be 2D
-	float vertices[] = {
-		0.5f,  0.5f, 0.0f,  // top right
-		0.5f, -0.5f, 0.0f,  // bottom right
-		-0.5f, -0.5f, 0.0f,  // bottom left
-		-0.5f,  0.5f, 0.0f,   // top left
-	};
+		// delete shader objects after linking
+		glDeleteShader(vertexShader);
+		glDeleteShader(fragmentShader);
+		glDeleteShader(yellowFragmentShader);
 
-	// exercise 1 second triangle (right-angled?)
-	float secondTriangleVertices[] = {
-		0.9f, 0.9f, 0.0f,
-		0.9f, 0.6f, 0.0f,
-		-0.9f, 0.9f, 0.0f
-	};
-	
-	// labels vertices with an index starting from 0 w.r.t the array of indices listed above
-	unsigned int indices[] = {
-		0, 1, 3,	// first triangle
-		1, 2, 3,	// second triangle
-		4, 5, 6
-	};
+		// vertex data for input to shader pipeline, renders a triangle
+		// data is x,y,z coordinate space, values must be between -1.0 and 1.0
+		// coordinate space is normalised (i.e all values > 1 or < -1 will be outside the screen space) and is converted to screen-space coordinates via the viewport transform function
+		// z coordinate is 0.0f as it wants to be 2D
+		float vertices[] = {
+			0.5f,  0.5f, 0.0f,  // top right
+			0.5f, -0.5f, 0.0f,  // bottom right
+			-0.5f, -0.5f, 0.0f,  // bottom left
+			-0.5f,  0.5f, 0.0f,   // top left
+		};
 
-	// vertex array object (VAO) stores calls to vertex attributes and VBOs to reduce multiple calls
-	unsigned int VAO, VBO, VAO2, VBO2;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
+		// exercise 1 second triangle (right-angled?)
+		float secondTriangleVertices[] = {
+			0.9f, 0.9f, 0.0f,
+			0.9f, 0.6f, 0.0f,
+			-0.9f, 0.9f, 0.0f
+		};
 
-	glBindVertexArray(VAO);
+		// labels vertices with an index starting from 0 w.r.t the array of indices listed above
+		unsigned int indices[] = {
+			0, 1, 3,	// first triangle
+			1, 2, 3,	// second triangle
+			4, 5, 6
+		};
 
-	// element buffer objects
-	unsigned int EBO;
-	glGenBuffers(1, &EBO);
+		// vertex array object (VAO) stores calls to vertex attributes and VBOs to reduce multiple calls
+		unsigned int VAO, VBO, VAO2, VBO2;
+		glGenVertexArrays(1, &VAO);
+		glGenBuffers(1, &VBO);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-	// vertex buffer object (VBO) storres vertices in memory and is sent in chunks to GPU
-	// is an opengl object and has a uid
-	// generate the buffer, bind buffer to appropriate target, copy user data into buffer with drawtype (static for unchanging, dynamic for updating data for performance)
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	// linking vertex attributes
-	// stride can be set to 0 if data is tightly packed (i.e contiguous data)
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
-
-	// ex2
-	glGenVertexArrays(1, &VAO2);
-	glGenBuffers(1, &VBO2);
-
-	glBindVertexArray(VAO2);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO2);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(secondTriangleVertices), secondTriangleVertices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-
-	// rendering loop
-	while (!glfwWindowShouldClose(window)) {
-		// input processing
-		processInput(window);
-
-		// rendering code
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-		// draw triangle
-		// ex3 uses new shader program with different fragment shader for yellow vs orange tris
-		glUseProgram(shaderProgramWithYellow);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3); // drawing from vertex array object
-		glBindVertexArray(VAO2);
-		glUseProgram(shaderProgram);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-		//glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+
+		// element buffer objects
+		unsigned int EBO;
+		glGenBuffers(1, &EBO);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+		// vertex buffer object (VBO) storres vertices in memory and is sent in chunks to GPU
+		// is an opengl object and has a uid
+		// generate the buffer, bind buffer to appropriate target, copy user data into buffer with drawtype (static for unchanging, dynamic for updating data for performance)
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+		// linking vertex attributes
+		// stride can be set to 0 if data is tightly packed (i.e contiguous data)
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(0);
+
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
 		glBindVertexArray(0);
 
-		// check & call events & swap buffers
-		glfwSwapBuffers(window);
-		glfwPollEvents();
+		// ex2
+		glGenVertexArrays(1, &VAO2);
+		glGenBuffers(1, &VBO2);
 
+		glBindVertexArray(VAO2);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO2);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(secondTriangleVertices), secondTriangleVertices, GL_STATIC_DRAW);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindVertexArray(0);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+
+		// rendering loop
+		while (!glfwWindowShouldClose(window)) {
+			// input processing
+			processInput(window);
+
+			// rendering code
+			glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT);
+			// draw triangle
+			// ex3 uses new shader program with different fragment shader for yellow vs orange tris
+			glUseProgram(shaderProgramWithYellow);
+			glBindVertexArray(VAO);
+			glDrawArrays(GL_TRIANGLES, 0, 3); // drawing from vertex array object
+			glBindVertexArray(VAO2);
+			glUseProgram(shaderProgram);
+			glDrawArrays(GL_TRIANGLES, 0, 3);
+			//glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+			glBindVertexArray(0);
+
+			// check & call events & swap buffers
+			glfwSwapBuffers(window);
+			glfwPollEvents();
+
+		}
+
+		glDeleteVertexArrays(1, &VAO);
+		glDeleteBuffers(1, &VBO);
+		glDeleteProgram(shaderProgram);
+
+		// glfw cleanup
+		glfwTerminate();
+		return 0;
 	}
 
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
-	glDeleteProgram(shaderProgram);
-
-	// glfw cleanup
-	glfwTerminate();
-	return 0;
-}
-
-// boilerplate
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-	glViewport(0, 0, width, height);
-}
-
-void error_callback(int error, const char* msg) {
-	std::string s;
-	s = " [" + std::to_string(error) + "] " + msg + '\n';
-	std::cerr << s << std::endl;
-}
-
-void processInput(GLFWwindow* window) {
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-		glfwSetWindowShouldClose(window, true);
+	// boilerplate
+	void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+		glViewport(0, 0, width, height);
 	}
 
-	if (glfwGetKey(window, GLFW_KEY_P) == GLFW_PRESS) {
-		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	void error_callback(int error, const char* msg) {
+		std::string s;
+		s = " [" + std::to_string(error) + "] " + msg + '\n';
+		std::cerr << s << std::endl;
 	}
 
-	if (glfwGetKey(window, GLFW_KEY_F) == GLFW_PRESS) {
-		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-	}
+	void processInput(GLFWwindow* window) {
+		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+			glfwSetWindowShouldClose(window, true);
+		}
 
+		if (glfwGetKey(window, GLFW_KEY_P) == GLFW_PRESS) {
+			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		}
+
+		if (glfwGetKey(window, GLFW_KEY_F) == GLFW_PRESS) {
+			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		}
+
+	}
 }
